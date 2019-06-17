7cce726b8f0ebbdec51bb055208089282f65ae7f
odegar <odegar@gmail.com>
2019-06-17 20:30:38 -0300

    Projeto de aula (Injeção de SQL)

---
+++ SqlInjection/Controllers/ContaController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SqlInjection.Controllers
+{
+    public class ContaController : Controller
+    {
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string usuario, string senha)
+        {
+            var connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ProdutosDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            //var consulta = "SELECT COUNT(*) FROM Usuarios WHERE Usuario = '" + usuario + "' AND Senha = '" + senha + "'";
+            var consulta = "SELECT Id FROM Usuarios WHERE Usuario = @usuario AND Senha = @senha;";
+
+            try
+            {
+                using (var conexao = new SqlConnection(connectionString))
+                {
+                    conexao.Open();
+
+                    using (SqlCommand comando = new SqlCommand(consulta, conexao))
+                    {
+
+                        comando.Parameters.Add(new SqlParameter("@usuario", usuario));
+                        comando.Parameters.Add(new SqlParameter("@senha", senha));
+
+                        var resultado = (int)comando.ExecuteScalar();
+                        if (resultado > 0)
+                        { 
+                            var cookie = new HttpCookie("idUsuario", resultado.ToString());
+                            Response.Cookies.Add(cookie);
+                            ViewBag.Mensagem = "Login efetuado com sucesso";
+                        }
+                        else
+                            ViewBag.Mensagem = "Falha no login";
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                ViewBag.Mensagem = "Erro: " + e.Message;
+            }
+
+            return View();
+        }
+    }
+}
---
+++ SqlInjection/Controllers/ProdutoController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SqlInjection.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        [HttpGet]
+        public ActionResult List(int? id)
+        {
+            var connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ProdutosDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            var consulta = "SELECT * FROM Produtos WHERE idUsuario = @idUsuario";
+
+            try
+            {
+                using (var conexao = new SqlConnection(connectionString))
+                {
+                    conexao.Open();
+
+                    using (SqlCommand comando = new SqlCommand(consulta, conexao))
+                    {
+                        if (id.HasValue)
+                        {
+                            comando.CommandText = consulta + " AND Id = @id;";
+                            comando.Parameters.Add(new SqlParameter("@id", id));
+                        }
+
+                        // Cookie
+                        var cookie = Request.Cookies["idUsuario"];
+                        if (cookie == null)
+                        {
+                            ViewBag.Mensagem = "Sessão expirada";
+                            return View();
+
+                        }
+                        comando.Parameters.Add(new SqlParameter("@idUsuario", cookie.Value));
+
+                        var leitor = comando.ExecuteReader();
+                        if (leitor.HasRows)
+                        {
+                            var produtos = new List<string>();
+                            while (leitor.Read())
+                            {
+                                produtos.Add(leitor["Descricao"].ToString());
+                            }
+                            ViewBag.Produtos = produtos;
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                ViewBag.Mensagem = "Erro: " + e.Message;
+            }
+
+            return View();
+        }
+    }
+}
